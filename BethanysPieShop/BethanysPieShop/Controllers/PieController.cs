02df2feaab0dfddaa1e05d4c82c442547648e73e
geologyrocks <Ed.Hughes93@gmail.com>
2020-02-12 09:06:50 +0000

    uodates

--- BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
@@ -20,7 +21,11 @@ namespace BethanysPieShop.Controllers
 
         public ViewResult List()
         {
-            return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
         }
     }
 }
