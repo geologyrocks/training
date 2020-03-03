e0ca93688fbc7ec69ad7a4b970c82adde4128c4c
geologyrocks <Ed.Hughes93@gmail.com>
2020-03-03 13:37:32 +0000

    Lambda-ise

--- Intermediate C#/CSharpDev/Student/Student-01-DelegatesEventsLambdas/BankSystem/BankSystem/Program.cs
+++ Intermediate C#/CSharpDev/Student/Student-01-DelegatesEventsLambdas/BankSystem/BankSystem/Program.cs
@@ -13,15 +13,10 @@ namespace BankSystem
             static void Main(string[] args)
             {
                 BankAccount acc1 = new BankAccount("Peter");
-                acc1.Overdrawn += delegate (object sender, BankAccountEventArgs e)
-                {
-                    Console.WriteLine($"You are overdrawn as of {e.Timestamp}.  Don't be.");
-                };
+                acc1.Overdrawn += (sender, e) => Console.WriteLine($"You are overdrawn as of {e.Timestamp}.  Don't be.");
 
-                acc1.ProtectionLimitExceeded += delegate (object sender, BankAccountEventArgs e)
-                {
-                    Console.WriteLine($"Account balances over £50,000 are not protected. Your balance is {e.AccountBalance} as of {e.Timestamp}");
-                };
+                acc1.ProtectionLimitExceeded += (sender, e) => Console.WriteLine($"Account balances over £50,000 are not protected. Your balance is {e.AccountBalance} as of {e.Timestamp}");
+                
                 // Do some stuff.
                 acc1.Deposit(10000);
                 acc1.Deposit(30000);
