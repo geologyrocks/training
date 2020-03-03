cffd4e5d69d729107b3b73b9270193f7940a332e
geologyrocks <Ed.Hughes93@gmail.com>
2020-03-03 14:03:43 +0000

    Bethany's Pie Shop

--- BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/PieController.cs
@@ -19,13 +19,28 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
+            IEnumerable<Pie> pies;
+            string currentCategory;
 
-            piesListViewModel.CurrentCategory = "Cheese cakes";
-            return View(piesListViewModel);
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
         public IActionResult Details(int id)
--- BethanysPieShop/BethanysPieShop/Startup.cs
+++ BethanysPieShop/BethanysPieShop/Startup.cs
@@ -28,7 +28,10 @@ namespace BethanysPieShop
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped<IPieRepository, PieRepository>();
-            services.AddScoped<ICategoryRepository, CategoryRepository>(); 
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
 
         }
@@ -43,6 +46,7 @@ namespace BethanysPieShop
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
