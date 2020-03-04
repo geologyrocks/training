28e4741c7118baec1b4e8cbf706e138c86a48897
geologyrocks <Ed.Hughes93@gmail.com>
2020-03-04 16:08:18 +0000

    Linq queries

---
+++ Intermediate-C#/CSharpDev/Student/Student-05-LinqObjects/UsingLinqCollections/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UsingLinqCollections
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            int[] ints = { 2,  4, 5, 3, 6, 1 };
+            var allQuery = from i in ints
+                         select i;
+            Output(allQuery);
+
+            var ascQuery = from i in ints
+                            orderby i
+                         select i;
+            Output(ascQuery);
+
+            var descQuery = from i in ints
+                         orderby i
+                         descending
+                         select i;
+            Output(descQuery);
+
+            var squaresQuery = from i in ints
+                         orderby i
+                         select i*i;
+            Output(squaresQuery);
+
+            var oddQuery = from i in ints
+                         orderby i
+                         where i % 2 != 0
+                         select i;
+            Output(oddQuery);
+
+            var sumQuery = ints.Sum();
+            Output(sumQuery);
+
+            var averageQuery = ints.Average();
+            Output(averageQuery);
+
+            var maxQuery = ints.Max();
+            Output(maxQuery);
+
+            var minQuery = ints.Min();
+            Output(minQuery);
+        }
+
+        private static void Output(IEnumerable<int> query)
+        {
+            Console.WriteLine("");
+            foreach (var part in query)
+            {
+                Console.WriteLine(part);
+            }
+        }
+        private static void Output(double query)
+        {
+            Console.WriteLine($"\n{query}");
+        }
+    }
+}
