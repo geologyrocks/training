fe0f60ba4ec55946a43d7814654bb47925153ca5
geologyrocks <Ed.Hughes93@gmail.com>
2020-03-04 12:11:18 +0000

    CyclicList

---
+++ Intermediate-C#/CSharpDev/Student/Student-04-Generics/CyclicList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CyclicListApp
+{
+    class CyclicList<T> : List<T>
+    {
+        private readonly int _maxLength;
+        private int _currentIndexPosition;
+        private List<T> _elements = new List<T>();
+        public CyclicList(int maxLength)
+        {
+            _maxLength = maxLength;
+            for (int i = 0; i < maxLength; i++)
+            {
+                _elements.Add(default(T));
+            }
+        }
+        public new void Add(T entry)
+        {
+            _currentIndexPosition = OverflowCheck(_currentIndexPosition);
+            _elements[_currentIndexPosition] = entry;
+            _currentIndexPosition++;
+        }
+
+        private int OverflowCheck(int positionToCheck)
+        {
+            return positionToCheck > _maxLength - 1 ? 0 : positionToCheck;
+        }
+
+        public T GetElement(int position)
+        {
+            try
+            {
+                Console.WriteLine($"Trying to get item at index {position}");
+                return _elements[position];
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine($"Position {position} is beyond the range of the list with length {_maxLength}");
+                return default;
+            }
+        }
+        public void GetAllElements()
+        {
+            var tempIndexPosition = _currentIndexPosition;
+            foreach (var item in _elements)
+            {
+                tempIndexPosition = OverflowCheck(tempIndexPosition);
+                Console.WriteLine(_elements[tempIndexPosition]);
+                tempIndexPosition++;
+            }
+        }
+    }
+}
---
+++ Intermediate-C#/CSharpDev/Student/Student-04-Generics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CyclicListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var cyclicList = new CyclicList<object>(7);
+            cyclicList.Add(1);
+            cyclicList.Add(2);
+            cyclicList.Add(3);
+            cyclicList.Add(4);
+            cyclicList.Add(5);
+            cyclicList.Add(6);
+            cyclicList.Add(7);
+            cyclicList.Add(8);
+            cyclicList.Add(9);
+            cyclicList.Add(10);
+            //Console.WriteLine(test.GetElement(0));
+            //Console.WriteLine(test.GetElement(5));
+            //Console.WriteLine(test.GetElement(6));
+            cyclicList.GetAllElements();
+        }
+    }
+}
