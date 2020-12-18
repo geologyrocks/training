fb226bfe91341002ccd6e8bcfefcdf9288724cc0
geologyrocks <Ed.Hughes93@gmail.com>
2020-12-18 17:03:25 +0000

    field check test passes

--- AdventOfCode/Day3/Program.cs
+++ AdventOfCode/Day3/Program.cs
@@ -7,40 +7,9 @@ namespace AdventOfCode
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
-            var text = File.ReadAllText(@"..\..\..\Input.txt");
-            Console.WriteLine(text);
-            var passportsList = text.Split(';');
-            var totalPassportsCount = passportsList.Count();
-            int validPassportsCount = 0;
-            int invalidPassportsCount = 0;
-            var permittedList = new List<string>()
-            {
-                "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"
-            };
-            foreach (var passportAsString in passportsList)
-            {
-                var passportFields = passportAsString.Split(',');
-                if (passportFields.Count() > 6)
-                {
-                    var intersection = permittedList.Intersect(passportFields);
-                    if (intersection.Count() > 6 )
-                    {
-                        validPassportsCount++;
-                    }
-                    else
-                    {
-                        invalidPassportsCount++;
-                    }
-                }
-                else
-                {
-                    invalidPassportsCount++;
-                }
-            }
-            Console.WriteLine($"Out of {totalPassportsCount} passports, {validPassportsCount} are valid, and {invalidPassportsCount} are invalid");
         }
     }
 }
--- AdventOfCode/Day3Tests/Day3Tests.cs
+++ AdventOfCode/Day3Tests/Day3Tests.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using FluentAssertions;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Day3Tests
@@ -20,15 +21,54 @@ namespace Day3Tests
             result.Should().BeOfType(typeof(List<string>));
         }
 
-        private List<string> GetPassports(string text)
+        [Fact]
+        public void FieldCheck_ReturnsCountOfFieldsInPassport()
         {
-            var textSplit = new List<string> (text.Split("\r\n\r\n"));
-            foreach (var passport in textSplit)
+            // Arrange
+            var text = File.ReadAllText(@"..\..\..\..\Day3\RawInput.txt");
+            var passports = GetPassports(text);
+
+            // Act
+            var (validCount, invalidCount) = FieldCheck(passports);
+
+            // Assert
+            validCount.Should().Be(242);
+            invalidCount.Should().Be(44);
+        }
+
+        private (int, int) FieldCheck(List<string> passports)
+        {
+            var validCount = 0;
+            var invalidCount = 0;
+            foreach (var passportString in passports)
             {
-                passport.Split(':');
+                var permittedFields = new List<string>()
+                {
+                    "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"
+                };
+                var passportFields = new List<string>(passportString.Split(':'));
+                var cleanPassportFields = new List<string>();
+                passportFields.RemoveAt(passportFields.Count - 1);
+                foreach (var field in passportFields)
+                {
+                    cleanPassportFields.Add(field.Substring(field.Length -3));
+                }
+                if (cleanPassportFields.Intersect(permittedFields).Count() >= 7)
+                {
+                    validCount++;
+                }
+                else
+                {
+                    invalidCount++;
+                }
             }
 
-            return textSplit;
+            return (validCount, invalidCount);
+        }
+
+        private List<string> GetPassports(string text)
+        {
+            return new List<string> (text.Split("\r\n\r\n"));
         }
     }
 }
